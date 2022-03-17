ef80104b43b9a2f820549ba85c0fe03a33d6e2db
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2022-03-17 16:49:41 +0100

    precommit changes

--- app/routers/triplestore.py
+++ app/routers/triplestore.py
@@ -1,5 +1,6 @@
 """Helper service for fetching triples."""
 from typing import Dict, List
+
 from fastapi import APIRouter
 from oteapi.models import TripleStoreConfig
 from oteapi.triplestore import TripleStore
@@ -12,8 +13,7 @@ async def fetch_query_result(
     triplestore_config: TripleStoreConfig,
     sparql_query: str,
 ) -> Dict[str, List[str]]:
-    """Connect to a triplestore and fetch mappings using sparql_query 
-    """
+    """Connect to a triplestore and fetch mappings using sparql_query"""
     triplestore_instance = TripleStore(triplestore_config)
     result = triplestore_instance.get(sparql_query)
     return result
