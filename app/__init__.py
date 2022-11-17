9c0c2c65bf0b78d9d204c6ff897fac01be8b116d
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2022-11-17 09:35:45 +0100

    Comply with PEP484

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ This repository contains the OTE-API server application.
 The server should be deployed via the `Dockerfile`.
 """
 
-__version__ = "1.20221028.276"
+__version__ = "1.20221117.278"
 __author__ = "SINTEF"
 __author_email__ = "Team4.0@SINTEF.no"
--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ from typing import TYPE_CHECKING
 from invoke import task
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import Tuple
+    from typing import Optional, Tuple
 
     from dulwich.objects import Commit
 
@@ -21,7 +21,9 @@ if TYPE_CHECKING:  # pragma: no cover
 TOP_DIR = Path(__file__).parent.resolve()
 
 
-def update_file(filename: Path, sub_line: "Tuple[str, str]", strip: str = None) -> None:
+def update_file(
+    filename: Path, sub_line: "Tuple[str, str]", strip: "Optional[str]" = None
+) -> None:
     """Utility function for tasks to read, update, and write files.
 
     Parameters:
