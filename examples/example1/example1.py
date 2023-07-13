94b67ce1d182b4c33233a8baf1aebeab85c8fad0
Treesa Joseph <66374813+Treesarj@users.noreply.github.com>
2023-07-13 11:58:03 +0200

    run precommit

--- examples/example1/example1.py
+++ examples/example1/example1.py
@@ -7,24 +7,24 @@ thisdir = Path(__file__).resolve().parent
 dlite.storage_path.append(thisdir)
 
 
-#client = OTEClient('python')  # for testing
-client = OTEClient('http://localhost:8080')  # replace with correct url
+# client = OTEClient('python')  # for testing
+client = OTEClient("http://localhost:8080")  # replace with correct url
 url = f"data.xlsx"
 
 dataresource = client.create_dataresource(
-     downloadUrl=Path("/app/examples/example1/data.xlsx").as_uri(),
-     mediaType="application/vnd.dlite-xlsx",
-     configuration={
-         'excel_config':{
-             'worksheet':'PhysicalParameters',
-             "header_row": "1",
-             "row_from": "2",
-             "row_to":"4"
-         },
-         "metadata": "http://onto-ns.com/meta/0.1/PhysicalProperties" ,
-         "storage_path": Path("/app/examples/example1/datamodel.json").as_uri(),
-         "label": "Physical_properties",
-     }
+    downloadUrl=Path("/app/examples/example1/data.xlsx").as_uri(),
+    mediaType="application/vnd.dlite-xlsx",
+    configuration={
+        "excel_config": {
+            "worksheet": "PhysicalParameters",
+            "header_row": "1",
+            "row_from": "2",
+            "row_to": "4",
+        },
+        "metadata": "http://onto-ns.com/meta/0.1/PhysicalProperties",
+        "storage_path": Path("/app/examples/example1/datamodel.json").as_uri(),
+        "label": "Physical_properties",
+    },
 )
 
 generator = client.create_function(
@@ -36,9 +36,6 @@ generator = client.create_function(
         "label": "Physical_properties",
     },
 )
-
-print("!!!!")
-print(thisdir)
 
 pipeline = dataresource >> generator
 
